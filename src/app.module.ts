64cd0ead35dc2a30159db64b94908bf45100076d
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-09 00:06:58 +0300

    feat:[master-xxx][vadim] : configure global settings and add Swagger documentation setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { CommonModule } from './common'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		CommonModule
+	],
 	controllers: [],
 	providers: []
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+import * as dotenv from 'dotenv'
+
+import { SwaggerModule } from '@nestjs/swagger'
+import { ApiResponseInterceptor, GlobalExceptionFilter } from './base'
+import { swagger } from './config'
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+
+	app.setGlobalPrefix('api')
+	app.useGlobalFilters(new GlobalExceptionFilter())
+	app.useGlobalInterceptors(new ApiResponseInterceptor())
+
+	SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, swagger))
+
+	await app.listen(process.env.PORT || 4200)
+
+	dotenv.config()
 }
 bootstrap()
